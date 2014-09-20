e5e9938f8a286f0744a602c11de8369f8ed19047
Adron Hall <adronhall@gmail.com>
2014-09-19 22:31:04 -0700

    Argh. I've got some first tests running.

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-var nools = require('nools');
-var ruleFilePath = __dirname + "/helloworld.nools";
-var flow = nools.compile();
-var Message = flow.getDefined("message");
+var nools = require ('nools');
+var ruleFilePath = __dirname + "/rules/helloworld.nools";
+var flow = nools.compile (ruleFilePath);
+var session = flow.getSession ();
 
-console.log("Rules executed @ " + ruleFilePath);
-console.log(Message);
+var Message = flow.getDefined ("message");
+
+console.log ("Rules executed @ " + ruleFilePath);
+
+session.assert (new Message ("hello"));
+session.assert (new Message ("hello or goodbye"));
+session.assert (new Message ("hello world"));
+session.assert (new Message ("goodbye"));
+
+console.log (session.getFacts ());
+
+session.match (function (err) {
+  if (err) {
+    console.error (err.stack);
+  } else {
+    console.log ("All done finding rules matches!");
+  }
+})
+
+session.getFacts (Number); //[1, 2];
+session.getFacts (String); //["A", "B"];
