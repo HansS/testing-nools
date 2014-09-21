0c883a163579ea8b5bc49d18e39eea55b8a1f2e6
Adron Hall <adronhall@gmail.com>
2014-09-21 03:19:28 -0700

    Cleaned up code to present on blog entry 2.

--- server.js
+++ server.js
@@ -5,34 +5,9 @@ var session = flow.getSession ();
 
 var Message = flow.getDefined ("message");
 
-//console.log ("Rules executed @ " + ruleFilePath);
-
 session.assert (new Message ("hello"));
 session.assert (new Message ("hello or goodbye"));
 session.assert (new Message ("hello world"));
 session.assert (new Message ("goodbye"));
 
 session.match();
-
-
-/* NOTE: This returns facts in a correlative way. Such as:
- *
- * goodbye
- * hello world goodbye
- * hello or goodbye
- * hello goodbye
- */
-//console.log (session.getFacts ());
-
-
-
-// session.match (function (err) {
-//   if (err) {
-//     console.error (err.stack);
-//   } else {
-//     console.log ("All done finding rules matches!");
-//   }
-// })
-
-// session.getFacts (Number);
-// session.getFacts (String);
